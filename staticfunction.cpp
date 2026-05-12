3cd9b3f63e5944a2a9b30003ac9938ab06e32c6f
variannn340 <varianvarian0301@gmail.com>
2026-05-12 10:25:42 +0700

    memberi nilai pada setNim() untuk merubah nilai NIM

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -44,5 +44,9 @@ int main() {
     //pembuatan object dan pemberian nilai
     Mahasiswa mhs1("Sri Dadi");
     Mahasiswa mhs2("Budi Datmiko");
-    
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
 }
