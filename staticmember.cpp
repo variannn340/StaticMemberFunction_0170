8f5ef6dea974c9dfc5e6be5412db5770af72619d
variannn340 <varianvarian0301@gmail.com>
2026-05-12 10:05:32 +0700

    ada tambahan

--- staticmember.cpp
+++ staticmember.cpp
@@ -26,3 +26,10 @@ public:
     void Mahasiswa::setID(){
         id = ++nim;
 };
+
+void Mahasiswa::printAll() {
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
