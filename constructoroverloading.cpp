8bce438721bb915f3d21dbfaeb38f630b344f382
variannn340 <varianvarian0301@gmail.com>
2026-05-12 11:30:24 +0700

    definisi hanya NIM dan Nama

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa{
+    private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; // definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama= " << nama << endl;
+}
