602aae8405a44a385c76e6d55aa943c00ed21e9a
variannn340 <varianvarian0301@gmail.com>
2026-05-12 10:13:53 +0700

    deklarasi class Mahasiswa

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -2,4 +2,17 @@
 #include <string> //menyertakan binary string
 using namespace std;
 
+class Mahasiswa {
 
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    
+
+}
