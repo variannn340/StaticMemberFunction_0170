6a966374c23be9a75729378d0455e597e6822d93
variannn340 <varianvarian0301@gmail.com>
2026-05-12 09:56:41 +0700

    declare class Mahasiswa

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+
+public:
+    static int nim; //deklarasi variabel static waktu yaitu variabel nim
+    //deklarasi variabel member
+    int 
+
+}
