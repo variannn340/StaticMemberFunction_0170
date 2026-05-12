2dd133c8041e466468b74a8b47dc7b8d0509d421
variannn340 <varianvarian0301@gmail.com>
2026-05-12 11:19:51 +0700

    chain function calls

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -13,3 +13,10 @@ class buku {
         return this->judul;
     }
 };
+
+int main()
+{
+    buku bukunya;
+    cout<<bukunya.setJudul("Matematika").getJudul(); //chain function calls
+    return 0; 
+}
